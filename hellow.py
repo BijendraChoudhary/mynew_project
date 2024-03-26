ee7801fcab5af7dcc0079b6b10e6168bf555e2e4
BijendraChoudhary <165006489+BijendraChoudhary@users.noreply.github.com>
2024-03-26 14:54:00 +0530

    Add files via upload

---
+++ hellow.py
@@ -0,0 +1,5 @@
+x=100
+if(x>200):
+    print("true")
+    Else
+    print("false")
---
+++ int.py
@@ -0,0 +1,20 @@
+'''
+x=input("Enter the first value:")
+y=input("Enter the second value:")
+sum=x+y
+print(sum)
+
+import math
+x=int(input("Enter the first value:"))
+y=int(input("Enter the second value:"))
+sum=x+y
+print(sum)
+print(math.sqrt(sum))
+'''
+import math
+r=int(input("Enter the Radius:"))
+#p=float(input("Enter the Pie:"))
+area=math.pi*r*r
+print(area)
+
+
